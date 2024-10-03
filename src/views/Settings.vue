6cf04c166f072e86b9475c026b569b8fe36c3c49
ybez <y.bezci@42-gmbh.de>
2024-10-03 18:15:32 +0200

    inital settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,22 +1,59 @@
 <template>
   <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>
+          <h2>Ayarlar</h2>
+        </ion-title>
+      </ion-toolbar>
+    </ion-header>
 
     <ion-content>
-      <h2>Ayarlar</h2>
+      <div >
+        <ion-card style="margin-bottom: 1rem;">
+ 
+      <ion-item>
+        <ion-label>Username</ion-label>
+        <ion-input slot="end" v-model="user.username"></ion-input>
+      </ion-item> 
+      <ion-item>
+        <ion-label>User number</ion-label>
+        <ion-input slot="end" v-model="user.usernumber"></ion-input>
+      </ion-item> 
       <ion-item>
         <ion-label>Bildirimler</ion-label>
-        <ion-toggle v-model="notificationsEnabled"></ion-toggle>
+        <ion-toggle slot="end" v-model="notificationsEnabled"></ion-toggle>
+      </ion-item> 
+      <ion-item>
+        <ion-label>Bildirimler</ion-label>
+        <ion-toggle slot="end" v-model="notificationsEnabled"></ion-toggle>
+      </ion-item> 
+      <ion-item>
+        <ion-label>Bildirimler</ion-label>
+        <ion-toggle slot="end" v-model="notificationsEnabled"></ion-toggle>
       </ion-item>
+    </ion-card>
+  </div>
     </ion-content>
   </ion-page>
 
 </template>
 
 <script>
+import { ref } from 'vue';
+
 export default {
-  data() {
+  setup() {
+    const user = ref({
+      username: 'user',
+      usernumber: 0
+    });
+
+    const notificationsEnabled = ref(true);
+
     return {
-      notificationsEnabled: true
+      user,
+      notificationsEnabled
     };
   }
 };
