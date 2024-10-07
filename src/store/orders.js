ed7751c109b7051e776a5a88bdeecd23b3c23af7
ybez <y.bezci@42-gmbh.de>
2024-10-08 01:07:48 +0200

    inital actions and historyOrders to store

--- src/store/orders.js
+++ src/store/orders.js
@@ -3,7 +3,8 @@ import { defineStore } from 'pinia';
 export const useOrderStore = defineStore('orders', {
   state: () => ({
     currentOrders: [],
-    completedOrders: []
+    completedOrders: [],
+    historyOrders: [] 
   }),
   actions: {
     addOrder(order) {
@@ -14,6 +15,19 @@ export const useOrderStore = defineStore('orders', {
       const order = this.currentOrders[orderIndex];
       this.currentOrders.splice(orderIndex, 1);
       this.completedOrders.push(order);
+    },
+    cancelOrder(orderId) {
+      const orderIndex = this.currentOrders.findIndex(order => order.id === orderId);
+      if (orderIndex !== -1) {
+        this.currentOrders.splice(orderIndex, 1); 
+      }
+    },
+    moveToHistory(orderId) {
+      const orderIndex = this.completedOrders.findIndex(order => order.id === orderId);
+      if (orderIndex !== -1) {
+        const [canceledOrder] = this.completedOrders.splice(orderIndex, 1);
+        this.historyOrders.push(canceledOrder); // İptal edilen sipariş history listesine taşınacak
+      }
     }
   }
 });
